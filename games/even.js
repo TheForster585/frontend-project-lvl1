41cc1c2c293f454bf290169d0f29fbaba9429b93
TheForster585 <Neonester@yandex.ru>
2021-03-30 17:03:57 +0300

    Simplify even.js

--- games/even.js
+++ games/even.js
@@ -7,13 +7,10 @@ const randomNumber = () => {
 
 const isEven = (number) => (number % 2 === 0);
 const expectedAnswer = (correctAnswer) => {
-  // eslint-disable-next-line default-case
-  switch (correctAnswer) {
-    case true:
-      return 'yes';
-    case false:
-      return 'no';
+  if (correctAnswer) {
+    return 'yes';
   }
+  return 'no';
 };
 
 const name = readlineSync.question(`Welcome to the Brain Games!
@@ -27,32 +24,12 @@ const game = () => {
     const currentNumber = randomNumber();
     const userAnswer = readlineSync.question(`Question: ${currentNumber}
 Your answer: `);
-    switch (userAnswer) {
-      case 'yes':
-        if (isEven(currentNumber)) {
-          console.log('Correct!');
-        } else {
-          console.log('\'yes\' is wrong answer ;(. Correct answer was \'no\'.');
-          console.log(`Let's try again, ${name}!`);
-          i = -1;
-        }
-        break;
-
-      case 'no':
-        if (!isEven(currentNumber)) {
-          console.log('Correct!');
-        } else {
-          console.log('\'no\' is wrong answer ;(. Correct answer was \'yes\'.');
-          console.log(`Let's try again, ${name}!`);
-          i = -1;
-        }
-        break;
-
-      default:
-        console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer(isEven(currentNumber))}'.`);
-        console.log(`Let's try again, ${name}!`);
-        i = -1;
-        break;
+    if (userAnswer === expectedAnswer(isEven(currentNumber))) {
+      console.log('Correct!');
+    } else {
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${expectedAnswer(isEven(currentNumber))}'.`);
+      console.log(`Let's try again, ${name}!`);
+      i = -1;
     }
   }
   console.log(`Congratulations, ${name}!`);
